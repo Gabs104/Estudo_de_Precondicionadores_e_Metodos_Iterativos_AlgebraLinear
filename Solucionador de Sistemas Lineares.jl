c5bc9231952f8e6b4d2a2665ad018a96cb367d5e
Quantikos <107156801+Gabs104@users.noreply.github.com>
2026-07-16 02:15:14 -0300

    Add files via upload

---
+++ Solucionador de Sistemas Lineares.jl
@@ -0,0 +1,343 @@
+# -------- IMPORTAÇÕES DE BIBLIOTECAS --------
+
+using LinearAlgebra # para importar os comandos, funções de algebra linear.
+using Random
+
+# ------------- ALGORITMO PARA RESOLVER SISTEMAS TRIANGULARES INFERIORES -------------------
+
+function sti(matriz, vetor) # o algoritmo de fato que resolve o sistema triangular inferior pelo algoritmo descoberto no livro: "Cálculo Numérico: aprendizagem com Apoio de Software"
+
+    # Toda vez que iniciarmos essa função definimos o vetor solução e o tamanho da matriz.
+
+    tamanho = size(matriz, 1) # fornece a o n° de linhas da matriz, como é quadrada o numero de colunas é igual.
+
+    vetor_sol = zeros(Float64, tamanho) # cria um vetor cheio de zeros com tamanho igual ao número de linhas da matriz que se esta resolvendo. Esse é o vetor que vai armazenar a solução.
+
+    for i in 1:tamanho # fazemos um laço for que depende do tamanho da matriz.
+
+        somatorio = 0.0 # para cada iteração, a soma linear muda.
+
+        for j in 1:i-1 # é o somatório de j indo de 1 até i-1 somando a_ij*x_j
+            somatorio += matriz[i, j] * vetor_sol[j]
+        end
+
+        # aqui calculamos o valor de x após ter realizado o somatório e armazenamos no vetor solução.
+        vetor_sol[i] = (vetor[i] - somatorio) / matriz[i, i]
+
+    end
+
+    return vetor_sol
+
+end
+
+# --------------- FIM DA FUNÇÃO ---------------
+
+
+# ------------------- ALGORITMO PARA RESOLVER SISTEMAS TRIANGULARES SUPERIORES -------------------------
+
+function sts(matriz, vetor) # o algoritmo de fato que resolve o sistema triangular superior pelo algoritmo descoberto no livro: "Cálculo Numérico: aprendizagem com Apoio de Software"
+
+    # toda vez que iniciamos essa função definimos a variável "tamanho" que recebe o tamanho da matriz e a variável "vetor_sol" que recebe o nosso vetor x solução.
+
+    tamanho = size(matriz, 1) # o size retorna quantas linhas tem essa matriz, se colocar o 2 ele retorna a quantidade de colunas.
+
+    vetor_sol = zeros(Float64, tamanho) # ele é composto com entradas de zeros que são do tipo float64.
+
+    for i in tamanho:-1:1 # laço for que vai diminuindo desde a n-ésima linha até a primeira linha.
+
+        somatorio = 0.0 # a soma linear que
+
+            for j in i+1:tamanho
+                somatorio += matriz[i,j] * vetor_sol[j]
+            end
+
+        vetor_sol[i] = (vetor[i]-somatorio)/matriz[i,i]
+
+    end
+
+    return vetor_sol
+
+end
+
+# -------------- FIM DA FUNÇÃO --------------
+
+# ------------ ALGORTIMO DA DECOMPOSIÇÃO LU COM USO DO TEOREMA DA DECOMPOSIÇÃO LU.
+
+function lu(matriz) # função responsável por verificar se é possivel decompor a matriz de coeficientes A em duas matrizes LU. Se possível, vai decompor a matriz A e mostrar as duas matrizes L e U obtidas.
+
+    # criamos as variáveis tamanho, menor_principal e continuar
+    # tamanho: recebe o tamanho da matriz quadrada inserida.
+    # menor_principal: cria uma matriz cheia de zeros de tamanho igual a tamanho-1
+
+    # ---------- VERIFICAÇÃO SE A MATRIZ INSERIDA É QUADRADA OU NÃO ----------------
+
+    linha = size(matriz, 1)
+    coluna = size(matriz, 2)
+
+    if linha != coluna
+        error("A matriz inserida não é quadrada e sim uma $linha x $coluna.")
+    end
+
+    # ---------- FIM DA VERIFICAÇÃO --------------
+
+    tamanho = size(matriz, 1)
+    matriz_l = Matrix{Float64}(I, tamanho, tamanho)
+    matriz_u = zeros(Float64, tamanho, tamanho)
+    println("Iniciando Decomposição...\n")
+
+    # ------------- ALGORITMO DE DECOMPOSIÇÃO LU -------------.
+
+    for i in 1:tamanho # iniciamos o processo como se estivessemos calculando um produto matricial usualmente, então escolhemos uma linha e realizamos o produto com todas as outras colunas.
+        
+        for j in 1:tamanho 
+
+            if i <= j && i == 1 # o caso em que i <= j e i = 1 que é quando os coeficientes u_1j é igual a a_1j.
+
+                matriz_u[i, j] = matriz[i, j]
+
+                if matriz_u[i,i] == 0 
+                    error("o elemento u_$i$i é nulo. Pelo Teorema da Decomposição LU, essa matriz não admite decomposição.")
+                end
+
+            elseif i <= j # segue para os próximos casos em que dependemos do valor l_ij. O algoritmo é igual a fórmula matemática feita.
+
+                somatorio_u = 0.0 # definimos a somatorio_u aqui para resetar toda vez que começar uma nova conta na próxima linha.
+
+                for k in 1:i-1
+
+                    somatorio_u += matriz_l[i,k]*matriz_u[k,j]
+
+                end
+
+                matriz_u[i, j] = matriz[i, j] - somatorio_u
+
+                if matriz_u[i,i] == 0
+                    error("o elemento u_$i$i é nulo. Pelo Teorema da Decomposição LU, essa matriz não admite decomposição.")
+                end
+
+            elseif i > j # o caso em que a linha é maior que a coluna. Neste caso é calculado o valor de l_ij pois é possível isolá-lo. O algoritmo é literalmente igual a fórmula feita matemáticamente.
+
+                somatorio_l = 0.0
+
+                for k in 1:j-1
+
+                    somatorio_l += matriz_l[i,k]*matriz_u[k,j]
+
+                end
+
+                matriz_l[i,j] = (matriz[i, j] - somatorio_l)/matriz_u[j, j]
+
+            end
+        end
+    end
+
+    
+
+    # -------------- FIM DO ALGORITMO ------------------.
+
+    # O programa finaliza e retorna as matrizes L e U cujo produto da a matriz A.
+
+    produto_lu = matriz_l*matriz_u # salvamos o produto LU.
+
+    if isapprox(norm(matriz - (produto_lu)), 0) == false # será se é uma boa comparação?
+
+        erro_rel = norm(matriz - (produto_lu))/norm(matriz) # a norma padrão da julia é a norm p = 2 que é igual a norma encontrada em bibliografias de álgebra linear. A norma de Frobenius.
+        println("O resultado do produto LU foi aproximado por um erro relativo de: $erro_rel\n")
+
+    end
+
+    println("---------Decomposição finalizada!---------\n")
+
+    return matriz_l, matriz_u # retorno das matrizes L e U para poder trabalhar com a solução via LU do sistema linear desejado.
+
+end
+
+# --------------- FIM DA FUNÇÃO --------------------
+
+# --------------- FUNÇÃO: Resolver o sistema linear Ax = b no formato (LU)x = b ------------------.
+
+# É necessário inserir informações como: A matriz triangular inferior L, A matriz triangular superior U e o vetor de coeficientes b.
+
+function resolver_lu(L, U, b)
+
+    # -------------- VERIFICAÇÕES ANTES DE INICIAR O PROCESSO DE SOLUÇÃO DE SISTEMA POR LU --------------
+
+    linha_b = size(b, 1)
+    linha_L = size(L, 1)
+
+
+    if linha_b != linha_L
+        error("o vetor b inserido tem dimensão $linha_b enquanto que a matriz L tem dimensão $linha_L\n Portanto, não é possível realizar o produto L^(-1) * b")
+    end
+
+
+    # ATENÇÃO !
+    # como estamos usando o determinante, acredito que seja mais eficiente usar apenas um checador se os elementos da diagonal principal não são nulos!
+
+    if det(U) == 0 
+        error("Como o determinante da matriz U é nula, não podemos realizar U^(-1) * y.\n Porntanto, impossível de resolver.")
+    end
+
+    # ---------------  FIM DAS VERIFICAÇÕES ----------------------
+
+    # --------------- ALGORITMO PARA RESOLVER SISTEMA LINEAR VIA LU. UTILIZAMOS AS FUNÇÕES STI() E STS() JÁ CRIADAS PARA APROVEITAR ------------------
+    
+    y = sti(LowerTriangular(L), b) # obtemos o vetor y para usar depois na função sts()
+    vetor_sol = sts(UpperTriangular(U), y) # aqui obtemos o vetor solução do sistema!
+
+    println("O vetor solução é: $vetor_sol")
+
+    return vetor_sol
+
+end
+
+# ---------------- FUNÇÃO: DECOMPOR A MATRIZ EM UM PRODUTO R^T * R (MÉTODO DE CHOLESKY) -----------------
+
+function chol(matriz)
+
+    println("Iniciando Decomposição via Cholesky....\n")
+
+    # PRIMEIRO CHECAMOS SE A MATRIZ INSERIDA É SIMÉTRICA OU NÃO. BASTA VERIFICAR SE A = A^T
+
+    matriz_t = transpose(matriz)
+
+    if isapprox(matriz_t, matriz) == false
+
+        error("A matriz inserida não é simétrica! A ≠ A^T.")
+
+    end
+
+    # APÓS A VERIFICAÇÃO, INICIAMOS O CÁLCULO DA MATRIZ R VIA MÉTODO DE CHOLESKY.
+
+    
+    tamanho = size(matriz, 1) # PEGAMOS A DIMENSÃO USANDO COMO REFERÊNCIA O N° DE LINHAS DA MATRIZ.
+    matriz_r = zeros(Float64, tamanho, tamanho)
+
+    # INICIAMOS O ALGORITMO.
+
+    for i in 1:tamanho 
+
+        for j in i:tamanho
+
+            if j == i && i == 1 # CASO i = j E i = 1, CASO EM QUE ESTAMOS CALCULANDO O ELEMENTO r_11.
+
+                if matriz[i, i] < 0 || matriz[i, i] == 0 
+
+                    r = matriz[i, i]
+
+                    error("O termo r_$i$i = $r é um valor não positivo. Portanto, não é possível fazer a decomposição via cholesky.")
+                
+                else
+
+                    matriz_r[i,i] = sqrt(matriz[i, i])
+
+                end
+
+            elseif j == i # CASO i = j ONDE O SOMATÓRIO APARECE. CASO EM QUE CALCULAMOS r_ii.
+
+                somatorio_1 = 0.0
+
+                for k in 1:i-1
+
+                    somatorio_1 += (matriz_r[k, i])^2
+
+                end
+
+                result = matriz[i, i] - somatorio_1
+
+                if result < 0 || result == 0
+
+                    error("O termo r_$i$i = $result é não positivo. Portanto, não é possível fazer a decomposição via cholesky.")
+
+                else
+
+                    matriz_r[i, i] = sqrt(result)
+
+                end
+
+            else    # CASO EM QUE CALCULAMOS r_ij
+
+                somatorio_2 = 0.0
+
+                for k in 1:i-1
+
+                    somatorio_2 += matriz_r[k, i]*matriz_r[k, j]
+
+                end
+
+                matriz_r[i, j] = (matriz[i, j] - somatorio_2)/matriz_r[i, i]
+
+            end
+
+        end
+
+    end
+
+    matriz_r_t = transpose(matriz_r)
+
+    println("-------------- Decomposição via Cholesky finalizada! ---------------\n")
+
+    return matriz_r, matriz_r_t
+
+end
+
+# ------------------ FIM DA FUNÇÃO ----------------------
+
+# ----------------- FUNÇÃO: RESOLVER SISTEMA LINEAR VIA CHOLESKY. IMPLEMENTAR? --------------------
+
+function resolver_chol(R, T, b)
+
+end
+
+# ---------------- FIM DA FUNÇÃO --------------------
+
+
+# -------------------- FUNÇÃO QUE TESTA lu() para matrizes variando de dimensão 1 até 100 ----------------------------
+
+function testar_lu()
+    
+    println("Iniciando teste dos algortimos se estão funcionando corretamente!!! \n Quantas iterações deseja?")
+
+    num_iteracoes = parse(Int64,readline())
+    loop = 1
+
+    while loop <= num_iteracoes
+
+        println("$loop ° iteração.\n")
+        # Vai ser escolhido um número de 1 até 100 para ser a dimensão da nossa matriz A.
+
+        linha = rand(1:100)
+        coluna = linha
+
+        # Criaremos a matriz A com valores entre -100 a 100 escolhidos aleatoriamente.
+        A = rand(-100.0:100.0, linha, coluna)
+
+        # criaremos o vetor b com valores entre -100 a 100 escolhidos aleatoriamente.
+        b = rand(-100.0:100.0, linha)
+
+        # começaremos pela decomposição LU para obter as matrizes L e U.
+
+        L, U = lu(A)
+
+        # Após isso, faremos a solução via LU. caso não for possível ja vai ser retornado o erro da própria função.
+
+        x = resolver_lu(L, U, b)
+
+        erro = norm(b - ((L*U)*x))  # é calculado a diferença entre o vetor b e o vetor (LU)x que é justamente o b. Isso é feito em norma!
+
+        print("o erro na $loop ° iteração foi de: $erro\n")
+        loop += 1
+
+    end
+
+    println("O programa foi finalizado!\n")
+
+end
+
+# ---------------- FIM DA FUNÇÃO ---------------
+
+# --------------- FUNÇÃO: MÉTODO ITERATIVO DE JACOBI ---------------------
+
+function jacobi()
+
+end
